4d7960e8f39f482e978dcc93e7fcc8de158a0e2a
Andrey <149412003+AndreyTkc@users.noreply.github.com>
2025-03-31 16:46:05 +0300

    final

--- MfGraphOrSomethingIdk/Form1.Designer.cs
+++ MfGraphOrSomethingIdk/Form1.Designer.cs
@@ -28,17 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea9 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend9 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series9 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.button2 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
@@ -50,18 +51,18 @@
             this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            chartArea1.Name = "ChartArea";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
+            chartArea9.Name = "ChartArea";
+            this.chart1.ChartAreas.Add(chartArea9);
+            legend9.Name = "Legend1";
+            this.chart1.Legends.Add(legend9);
             this.chart1.Location = new System.Drawing.Point(-5, -3);
-            this.chart1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.chart1.Margin = new System.Windows.Forms.Padding(4);
             this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Polar;
-            series1.Legend = "Legend1";
-            series1.Name = "Series";
-            this.chart1.Series.Add(series1);
+            series9.ChartArea = "ChartArea";
+            series9.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Polar;
+            series9.Legend = "Legend1";
+            series9.Name = "Series";
+            this.chart1.Series.Add(series9);
             this.chart1.Size = new System.Drawing.Size(988, 560);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "text";
@@ -70,20 +71,44 @@
             // groupBox1
             // 
             this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.button3);
             this.groupBox1.Controls.Add(this.textBox1);
             this.groupBox1.Controls.Add(this.textBox2);
-            this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.numericUpDown2);
             this.groupBox1.Controls.Add(this.numericUpDown1);
-            this.groupBox1.Controls.Add(this.button2);
-            this.groupBox1.Location = new System.Drawing.Point(720, 391);
-            this.groupBox1.MaximumSize = new System.Drawing.Size(250, 150);
+            this.groupBox1.Location = new System.Drawing.Point(720, 41);
+            this.groupBox1.MaximumSize = new System.Drawing.Size(250, 500);
             this.groupBox1.MinimumSize = new System.Drawing.Size(250, 150);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(250, 150);
+            this.groupBox1.Size = new System.Drawing.Size(250, 500);
             this.groupBox1.TabIndex = 8;
             this.groupBox1.TabStop = false;
             this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox3.Enabled = false;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(21, 149);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(203, 26);
+            this.textBox3.TabIndex = 16;
+            this.textBox3.Text = "Change color";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(21, 181);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(203, 37);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Select color";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // textBox1
             // 
@@ -111,15 +136,6 @@
             this.textBox2.Text = "Change a value";
             this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(130, 113);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 26);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "Save";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // numericUpDown2
             // 
             this.numericUpDown2.DecimalPlaces = 1;
@@ -140,13 +156,14 @@
             0,
             65536});
             this.numericUpDown2.Name = "numericUpDown2";
-            this.numericUpDown2.Size = new System.Drawing.Size(103, 22);
+            this.numericUpDown2.Size = new System.Drawing.Size(203, 22);
             this.numericUpDown2.TabIndex = 12;
             this.numericUpDown2.Value = new decimal(new int[] {
             1,
             0,
             0,
             0});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
             // 
             // numericUpDown1
             // 
@@ -168,22 +185,14 @@
             0,
             65536});
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(103, 22);
+            this.numericUpDown1.Size = new System.Drawing.Size(203, 22);
             this.numericUpDown1.TabIndex = 9;
             this.numericUpDown1.Value = new decimal(new int[] {
             2,
             0,
             0,
             65536});
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(130, 49);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(94, 26);
-            this.button2.TabIndex = 11;
-            this.button2.Text = "Save";
-            this.button2.UseVisualStyleBackColor = true;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
             // 
             // Form1
             // 
@@ -192,7 +201,7 @@
             this.ClientSize = new System.Drawing.Size(982, 553);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.chart1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MinimumSize = new System.Drawing.Size(1000, 600);
             this.Name = "Form1";
             this.Text = "Logarithmic Spiral";
@@ -212,10 +221,11 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.NumericUpDown numericUpDown2;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ColorDialog colorDialog1;
     }
 }
 
--- MfGraphOrSomethingIdk/Form1.cs
+++ MfGraphOrSomethingIdk/Form1.cs
@@ -14,6 +14,9 @@ namespace MfGraphOrSomethingIdk
 {
     public partial class Form1 : Form
     {
+		private Color spiralColor = Color.Gray;
+		private double a = 1.0;
+		private double b = 0.2;
         public Form1()
         {
             InitializeComponent();
@@ -25,48 +28,36 @@ namespace MfGraphOrSomethingIdk
         }
 		private void Form1_Load(object sender, EventArgs e)
 		{
-			// Set up the chart as a polar chart
 			chart1.Series.Clear();
 			chart1.ChartAreas[0].Area3DStyle.Enable3D = false;
 
-            // Configure the angle axis (AxisX)
-            chart1.ChartAreas[0].AxisX.LabelStyle.Enabled = true; // Show angle labels
-			chart1.ChartAreas[0].AxisX.Interval = 30; // Set angle interval to 30 degrees
-			chart1.ChartAreas[0].AxisX.Minimum = 0; // Start angle at 0 degrees
-			chart1.ChartAreas[0].AxisX.Maximum = 360; // End angle at 360 degrees
-			chart1.ChartAreas[0].AxisX.Crossing = 0; // Ensure 0 degrees is at the rightmost point
+            chart1.ChartAreas[0].AxisX.LabelStyle.Enabled = true;
+			chart1.ChartAreas[0].AxisX.Interval = 30;
+			chart1.ChartAreas[0].AxisX.Minimum = 0;
+			chart1.ChartAreas[0].AxisX.Maximum = 360;
+			chart1.ChartAreas[0].AxisX.Crossing = 0;
 
-			// Configure the radial axis (AxisY)
-			chart1.ChartAreas[0].AxisY.LabelStyle.Enabled = true; // Show radial labels
-			chart1.ChartAreas[0].AxisY.Minimum = 0; // Start radius at 0
-			chart1.ChartAreas[0].AxisY.Maximum = 10; // Set maximum radius (adjust as needed)
+			chart1.ChartAreas[0].AxisY.LabelStyle.Enabled = true;
+			chart1.ChartAreas[0].AxisY.Minimum = 0;
+			chart1.ChartAreas[0].AxisY.Maximum = 10;
 
-			// Add a series for the logarithmic spiral
 			Series spiralSeries = new Series("Logarithmic Spiral");
 			spiralSeries.ChartType = SeriesChartType.Polar;
-            spiralSeries.Color = Color.Red; // Change the line color
-            spiralSeries.BorderWidth = 2; // Set the line thickness
+            spiralSeries.Color = spiralColor;
+            spiralSeries.BorderWidth = 2;
             chart1.Series.Add(spiralSeries);
 
-            // Generate the logarithmic spiral data points
             GenerateLogarithmicSpiral(spiralSeries);
 		}
 
 		private void GenerateLogarithmicSpiral(Series series)
 		{
-			// Constants for the logarithmic spiral
-			double a = 1.0; // Controls the starting radius
-			double b = 0.2; // Controls the tightness of the spiral
-
-			// Clear any existing data points
 			series.Points.Clear();
 
-			// Generate points for the logarithmic spiral
 			for (double theta = 0; theta <= 10 * Math.PI; theta += 0.1)
 			{
 				double r = a * Math.Exp(b * theta); // Logarithmic spiral equation
 
-				// Convert theta from radians to degrees (if needed)
 				double thetaDegrees = theta * (180 / Math.PI);
 
 				series.Points.AddXY(thetaDegrees, r);
@@ -75,7 +66,7 @@ namespace MfGraphOrSomethingIdk
 			
 		private void chart1_Click(object sender, EventArgs e)
 		{
-			// Optional: Regenerate the spiral on click if needed
+            // Optional
 			GenerateLogarithmicSpiral(chart1.Series["Logarithmic Spiral"]);
 		}
 
@@ -87,6 +78,39 @@ namespace MfGraphOrSomethingIdk
         private void groupBox1_Enter(object sender, EventArgs e)
         {
 
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+			if (colorDialog1.ShowDialog() == DialogResult.OK)
+			{
+				Color color = colorDialog1.Color;
+				spiralColor = color;
+                Form1_Load(sender, e);
+            }
+
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+			a = (double)numericUpDown2.Value;
+            Form1_Load(sender, e);
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            b = (double)numericUpDown1.Value;
+            Form1_Load(sender, e);
         }
     }
 }
